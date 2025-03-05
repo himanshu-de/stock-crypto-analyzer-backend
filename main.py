6a7555ea6afac145515a6eb1cc270ef8d519e6d0
himanshu-de <makeshsingh1234@gmail.com>
2025-03-05 10:06:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask on Render!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))  # Render default port 10000 hai
+    app.run(host='0.0.0.0', port=port)
+
 from fastapi import FastAPI
 import yfinance as yf
 
